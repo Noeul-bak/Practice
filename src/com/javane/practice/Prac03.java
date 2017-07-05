34992680e7d03d4bb9f546e2fa764a1243faabda
R33 <R33@pc>
2017-07-05 13:55:25 +0900

    Prac 1,2,3완료

---
+++ src/com/javane/practice/Prac03.java
@@ -0,0 +1,14 @@
+package com.javane.practice;
+
+public class Prac03 {
+	public static void main(String[] args) {
+
+		for(int y=1;y<=9;y++) {
+			for(int x=2;x<=9;x++) {
+				System.out.print(x+"*"+y+"="+x*y+"\t");
+			}
+			System.out.println();
+		}
+	}
+
+}
